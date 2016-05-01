1c4ac74ef8584ceb5b249e54894085b577dea1b6
Piotr.Mazurek <masourek@gmail.com>
2016-05-01 20:46:03 +0100

    Adds pypi version

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     url='https://github.com/pmazurek/aws-fuzzy-finder',
     description='SSH into AWS instances using fuzzy search through tags.',
+    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.1',
     author='Piotr Mazurek, Daria Rudkiewicz',
     keywords=['aws', 'ssh', 'fuzzy', 'ec2'],
     packages=['aws_fuzzy_finder'],
