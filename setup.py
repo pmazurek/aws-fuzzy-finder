1ae13e0d7ec839b19d0c54793387610ff7d449b3
Piotr.Mazurek <masourek@gmail.com>
2016-05-02 21:55:13 +0100

    Releasing new version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='aws-fuzzy-finder',
-    version='0.1',
+    version='0.2',
     url='https://github.com/pmazurek/aws-fuzzy-finder',
     description='SSH into AWS instances using fuzzy search through tags.',
-    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.1',
+    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.2',
     author='Piotr Mazurek, Daria Rudkiewicz',
     keywords=['aws', 'ssh', 'fuzzy', 'ec2'],
     packages=['aws_fuzzy_finder'],
