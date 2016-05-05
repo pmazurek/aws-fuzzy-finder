995691f95294b4a7c42a420a213f8429ca4491a1
Piotr.Mazurek <masourek@gmail.com>
2016-05-05 10:24:06 +0100

    Adding macos package

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='aws-fuzzy-finder',
-    version='0.3',
+    version='0.3.1',
     url='https://github.com/pmazurek/aws-fuzzy-finder',
     description='SSH into AWS instances using fuzzy search through tags.',
-    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.3',
+    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/0.3.1',
     author='Piotr Mazurek, Daria Rudkiewicz',
     keywords=['aws', 'ssh', 'fuzzy', 'ec2'],
     packages=['aws_fuzzy_finder'],
-    package_data={'': ['fzf-0.12.1-linux_386']},
+    package_data={'': ['fzf-0.12.1-linux_386', 'fzf-0.12.1-darwin_386']},
     install_requires=[
         'boto3==1.3.1',
         'click==6.6',
