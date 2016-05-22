b0ced54eec895d7a5d64b7235f70d53e33d67d08
Piotr.Mazurek <masourek@gmail.com>
2016-05-22 15:57:20 +0100

    #6 Adds --ip-only param.

--- aws_fuzzy_finder/main.py
+++ aws_fuzzy_finder/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 import click
 
 from .aws_utils import (
@@ -19,7 +20,8 @@ from .settings import (
 @click.option('--private', 'use_private_ip', flag_value=True, help="Use private IP's")
 @click.option('--key-path', default='~/.ssh/id_rsa', help="Path to your private key, default: ~/.ssh/id_rsa")
 @click.option('--user', default='ec2-user', help="User to SSH with, default: ec2-user")
-def entrypoint(use_private_ip, key_path, user):
+@click.option('--ip-only', 'ip_only', flag_value=True, help="Print chosen IP to STDOUT and exit")
+def entrypoint(use_private_ip, key_path, user, ip_only):
 
     boto_instance_data = get_aws_instances()
     searchable_instances = prepare_searchable_instances(
@@ -42,13 +44,19 @@ def entrypoint(use_private_ip, key_path, user):
         exit(1)
 
     chosen_ip = choice.split(SEPARATOR)[1].rstrip()
-    ssh_command = ENV_SSH_COMMAND_TEMPLATE.format(
-        user=ENV_SSH_USER or user,
-        key=ENV_KEY_PATH or key_path,
-        host=chosen_ip,
-    )
-    print(ssh_command)
-    subprocess.call(ssh_command, shell=True, executable='/bin/bash')
+
+    if ip_only:
+        sys.stdout.write(chosen_ip)
+        exit(0)
+    else:
+        ssh_command = ENV_SSH_COMMAND_TEMPLATE.format(
+            user=ENV_SSH_USER or user,
+            key=ENV_KEY_PATH or key_path,
+            host=chosen_ip,
+        )
+        # print the ssh command before executing, so that it's clear what is being done
+        print(ssh_command)
+        subprocess.call(ssh_command, shell=True, executable='/bin/bash')
 
 if __name__ == '__main__':
     entrypoint()
