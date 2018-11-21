10a33370de1516c2b81197e8dcb60e3bf8d7d721
Piotr Mazurek <piotr.mazurek@incoproip.com>
2018-11-21 16:24:07 +0000

    Bumping version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='aws-fuzzy-finder',
-    version='0.3.6',
+    version='1.0.0',
     url='https://github.com/pmazurek/aws-fuzzy-finder',
     description='SSH into AWS instances using fuzzy search through tags.',
-    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/v0.3.6',
+    download_url='https://github.com/pmazurek/aws-fuzzy-finder/tarball/v1.0.0',
     author='Piotr Mazurek, Daria Rudkiewicz',
     keywords=['aws', 'ssh', 'fuzzy', 'ec2'],
     packages=['aws_fuzzy_finder'],
